605f1f4b854f45159a3265889bf9b0807811716c
sudhanshu jandial <sudhanshu4ucool@outlook.com>
2021-07-17 14:40:18 +0530

    entitiframework components added

---
+++ JWTAuthentication/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace JWTAuthentication.Data
+{
+    using JWTAuthentication.Entities;
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
--- JWTAuthentication/Startup.cs
+++ JWTAuthentication/Startup.cs
@@ -1,7 +1,9 @@
+using JWTAuthentication.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,11 @@ namespace JWTAuthentication
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite("connection string");
+
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
