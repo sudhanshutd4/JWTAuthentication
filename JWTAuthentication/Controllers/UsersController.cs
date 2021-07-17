e37b03ca361adb3d19981d8996722c5907eba583
sudhanshu jandial <sudhanshu4ucool@outlook.com>
2021-07-17 15:48:15 +0530

    Users Controller Added

---
+++ JWTAuthentication/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿namespace JWTAuthentication.Controllers
+{
+    using JWTAuthentication.Data;
+    using JWTAuthentication.Entities;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();          
+        }
+
+        //api/users/id
+        [HttpGet("{id}")]
+        public async Task <ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+       
+        }
+    }
+}
