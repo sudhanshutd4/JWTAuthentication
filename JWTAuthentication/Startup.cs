79c5271bc456b888921fea1a80b9d6ddc0b40c4e
sudhanshu jandial <sudhanshu4ucool@outlook.com>
2021-07-18 20:15:45 +0530

    comments added

--- JWTAuthentication/Startup.cs
+++ JWTAuthentication/Startup.cs
@@ -35,16 +35,21 @@ namespace JWTAuthentication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<ITokenService,TokenService>();
+            #region database
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
 
             });
+            #endregion
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTAuthentication", Version = "v1" });
             });
+
+            #region Authentication
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -56,6 +61,7 @@ namespace JWTAuthentication
                         ValidateAudience = false,
                     };
                 });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
